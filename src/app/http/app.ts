a2ad96f821925e8d21d1f71bc6a6473fd3e294bb
sob-user <oulad.b.s@gmail.com>
2021-05-12 16:34:19 +0200

    implement swagger documentation

--- src/app/http/app.ts
+++ src/app/http/app.ts
@@ -3,6 +3,9 @@ import morgan from 'morgan';
 import { API_PORT, API_BASE_URL } from '../../env/variables';
 import { v1Router } from './router';
 
+import swaggerConfig from '../../middleware/documentation/swagger.json'
+import swaggerUi from 'swagger-ui-express'
+
 export const createServer = async function(): Promise<express.Application> {
     
     const app: express.Application = express();
@@ -11,6 +14,8 @@ export const createServer = async function(): Promise<express.Application> {
     app.use(morgan('dev'));
 
     app.use(`${API_BASE_URL}`, v1Router);
+
+    app.use(`${API_BASE_URL}documentation/`, swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
     app.listen(API_PORT, function() {
         console.log('[API]: started [PORT]: ' + API_PORT);
