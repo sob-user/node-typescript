5087e3200d37b841ec912a7ba3c57945f74c343b
sob-user <oulad.b.s@gmail.com>
2021-05-13 16:23:06 +0200

    implement user in entities

---
+++ src/app/database/typeorm/entities/index.ts
@@ -0,0 +1,8 @@
+import { User } from './user';
+// import { Skill } from './skill';
+// import { Category } from './category';
+
+const entities = [User]
+const entitiesObject = { User }
+
+export { entities, entitiesObject }
---
+++ src/app/database/typeorm/entities/user.ts
@@ -0,0 +1,16 @@
+import { Entity,  PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@Entity()
+export class  User  extends BaseEntity  {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    isActive: boolean;
+}
