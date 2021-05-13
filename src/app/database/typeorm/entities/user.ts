e67460bd607843a957253eec3274ab8d9dea1709
sob-user <oulad.b.s@gmail.com>
2021-05-13 16:55:34 +0200

    add options in user entity

--- src/app/database/typeorm/entities/user.ts
+++ src/app/database/typeorm/entities/user.ts
@@ -1,16 +1,28 @@
 import { Entity,  PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+const mostLongestFirstName: number = 1000;
+const mostLongestLastName: number = 666;
+
+
 @Entity()
 export class  User  extends BaseEntity  {
     @PrimaryGeneratedColumn("uuid")
     id: string;
 
-    @Column()
+    @Column({
+        length: mostLongestFirstName
+    })
     firstName: string;
 
-    @Column()
+    @Column({
+        length: mostLongestLastName
+    })
     lastName: string;
 
     @Column()
     isActive: boolean;
+
+    @Column("time with time zone")
+    addedAt: Date;
+
 }
